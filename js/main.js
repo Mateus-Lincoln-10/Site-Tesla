948d585e5c932cbe03e9c7976783f22f0735f7c1
Mateus lincoln <lincolnsoares52@gmail.com>
2022-04-17 18:29:00 -0300

    implementando login

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let menu = document.querySelector('.button-menu');
 let closeMenu = document.querySelector('#close-menu');
 let openMenu = document.querySelector('.header-responsive');
+let openForm = document.querySelector('#account-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
 
 menu.onclick = () => {
   openMenu.classList.add('active');
@@ -8,4 +11,12 @@ menu.onclick = () => {
 
 closeMenu.onclick = () => {
   openMenu.classList.remove('active');
+}
+
+openForm.onclick = () => {
+  loginForm.classList.add('active');
+}
+
+formClose.onclick = () => {
+  loginForm.classList.remove('active');
 }
